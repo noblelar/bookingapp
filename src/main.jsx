41a066bba53b92c694a0c86405942842511e9874
noblelar <59696875+noblelar@users.noreply.github.com>
2023-10-09 21:38:21 +0100

    BKappWorkFlow

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import * as React from "react";
+// import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+// ReactDOM.createRoot(document.getElementById("root")).render(
+
+root.render(
   // <React.StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true }}>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter future={{ v7_startTransition: true }}>
+    <App />
+  </BrowserRouter>
   // </React.StrictMode>
 );
